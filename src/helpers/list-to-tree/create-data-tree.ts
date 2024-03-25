f72b07b30d3ef1e7b6ff168d37b3651a5280e894
v-nvtsk <v@nvtsk.ru>
2024-03-25 16:49:51 +0300

    feat(helpers): add list-to-tree helper

---
+++ src/helpers/list-to-tree/create-data-tree.ts
@@ -0,0 +1,15 @@
+import { Category } from "../../api/expenses-api";
+
+export const createDataTree = (list: Partial<Category>[]) => {
+  const hashTable = Object.create(null);
+  list.forEach((elem) => {
+    hashTable[elem.id!] = { ...elem, children: [] };
+  });
+
+  const dataTree: Category[] = [];
+  list.forEach((elem) => {
+    if (elem.parentId) hashTable[elem.parentId].children.push(hashTable[elem.id!]);
+    else dataTree.push(hashTable[elem.id!]);
+  });
+  return dataTree;
+};
---
+++ src/helpers/list-to-tree/index.test.ts
@@ -0,0 +1,152 @@
+import { Category } from "../../api/expenses-api";
+import { createDataTree } from "./index";
+
+describe("createDataTree", () => {
+  test("test1", () => {
+    const list: Partial<Category>[] = [
+      { id: "2", name: "Электроника", parentId: "" },
+      { id: "3", name: "Телефоны", parentId: "2" },
+      { id: "8", name: "Книги", parentId: "" },
+      { id: "9", name: "Учебники", parentId: "8" },
+      { id: "6", name: "Ноутбуки", parentId: "2" },
+      { id: "7", name: "Телевизоры", parentId: "2" },
+      { id: "10", name: "Художественная", parentId: "8" },
+      { id: "11", name: "Комиксы", parentId: "8" },
+      { id: "4", name: "Смартфоны", parentId: "3" },
+      { id: "5", name: "Аксессуары", parentId: "3" },
+    ];
+
+    expect(createDataTree(list)).toEqual([
+      {
+        id: "2",
+        name: "Электроника",
+        parentId: "",
+        children: [
+          {
+            id: "3",
+            name: "Телефоны",
+            parentId: "2",
+            children: [
+              { id: "4", name: "Смартфоны", parentId: "3", children: [] },
+              { id: "5", name: "Аксессуары", parentId: "3", children: [] },
+            ],
+          },
+          { id: "6", name: "Ноутбуки", parentId: "2", children: [] },
+          { id: "7", name: "Телевизоры", parentId: "2", children: [] },
+        ],
+      },
+      {
+        id: "8",
+        name: "Книги",
+        parentId: "",
+        children: [
+          { id: "9", name: "Учебники", parentId: "8", children: [] },
+          { id: "10", name: "Художественная", parentId: "8", children: [] },
+          { id: "11", name: "Комиксы", parentId: "8", children: [] },
+        ],
+      },
+    ]);
+  });
+
+  test("test2", () => {
+    const list = [
+      { id: "0", name: "Сюрприз =)", parentId: "11" },
+      { id: "2", name: "Электроника", parentId: "" },
+      { id: "3", name: "Телефоны", parentId: "2" },
+      { id: "8", name: "Книги", parentId: "" },
+      { id: "9", name: "Учебники", parentId: "8" },
+      { id: "6", name: "Ноутбуки", parentId: "2" },
+      { id: "7", name: "Телевизоры", parentId: "2" },
+      { id: "10", name: "Художественная", parentId: "8" },
+      { id: "11", name: "Комиксы", parentId: "8" },
+      { id: "4", name: "Смартфоны", parentId: "3" },
+      { id: "5", name: "Аксессуары", parentId: "4" },
+    ];
+
+    expect(createDataTree(list)).toEqual([
+      {
+        id: "2",
+        name: "Электроника",
+        parentId: "",
+        children: [
+          {
+            id: "3",
+            name: "Телефоны",
+            parentId: "2",
+            children: [
+              {
+                id: "4",
+                name: "Смартфоны",
+                parentId: "3",
+                children: [{ id: "5", name: "Аксессуары", parentId: "4", children: [] }],
+              },
+            ],
+          },
+          { id: "6", name: "Ноутбуки", parentId: "2", children: [] },
+          { id: "7", name: "Телевизоры", parentId: "2", children: [] },
+        ],
+      },
+      {
+        id: "8",
+        name: "Книги",
+        parentId: "",
+        children: [
+          { id: "9", name: "Учебники", parentId: "8", children: [] },
+          { id: "10", name: "Художественная", parentId: "8", children: [] },
+          {
+            id: "11",
+            name: "Комиксы",
+            parentId: "8",
+            children: [{ id: "0", name: "Сюрприз =)", parentId: "11", children: [] }],
+          },
+        ],
+      },
+    ]);
+  });
+
+  test("test3", () => {
+    const list = [
+      { id: "3", name: "Телефоны", parentId: "2" },
+      { id: "2", name: "Электроника", parentId: "" },
+      { id: "9", name: "Учебники", parentId: "8" },
+      { id: "8", name: "Книги", parentId: "" },
+      { id: "6", name: "Ноутбуки", parentId: "2" },
+      { id: "7", name: "Телевизоры", parentId: "2" },
+      { id: "10", name: "Художественная", parentId: "8" },
+      { id: "11", name: "Комиксы", parentId: "8" },
+      { id: "4", name: "Смартфоны", parentId: "3" },
+      { id: "5", name: "Аксессуары", parentId: "3" },
+    ];
+
+    expect(createDataTree(list)).toEqual([
+      {
+        id: "2",
+        name: "Электроника",
+        parentId: "",
+        children: [
+          {
+            id: "3",
+            name: "Телефоны",
+            parentId: "2",
+            children: [
+              { id: "4", name: "Смартфоны", parentId: "3", children: [] },
+              { id: "5", name: "Аксессуары", parentId: "3", children: [] },
+            ],
+          },
+          { id: "6", name: "Ноутбуки", parentId: "2", children: [] },
+          { id: "7", name: "Телевизоры", parentId: "2", children: [] },
+        ],
+      },
+      {
+        id: "8",
+        name: "Книги",
+        parentId: "",
+        children: [
+          { id: "9", name: "Учебники", parentId: "8", children: [] },
+          { id: "10", name: "Художественная", parentId: "8", children: [] },
+          { id: "11", name: "Комиксы", parentId: "8", children: [] },
+        ],
+      },
+    ]);
+  });
+});
