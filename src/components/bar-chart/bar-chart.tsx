cf257be6c859680766a6d9e4c037ef059586cf03
v-nvtsk <v@nvtsk.ru>
2024-03-26 23:15:54 +0300

    feat(component): add - bar chart

---
+++ src/components/bar-chart/bar-chart.tsx
@@ -0,0 +1,35 @@
+import React, { memo } from "react";
+import { Chart } from "react-google-charts";
+import { Expense } from "../../store/expenseSlice";
+import { Category } from "../../api/expenses-api";
+
+type BarChartData = {
+  categories: Category[];
+  expenses: Expense[];
+};
+
+type Props = {
+  data: BarChartData;
+};
+
+export const BarChart = memo(({ data }: Props) => {
+  if (data.expenses.length === 0) return null;
+
+  const processedData = [
+    ...data.categories
+      // .filter(c => c.parentId === "")
+      .map((category): [string, number] => {
+        const sum = data.expenses.reduce((acc, el) => {
+          if (el.categoryId === category.id) return acc + el.amount;
+          return acc;
+        }, 0);
+        return [category.name, sum / 100];
+      })
+      .filter((e) => e[1] > 0)
+      .sort((a: [string, number], b: [string, number]) => b[1] - a[1]),
+  ];
+
+  const sdata = [["category", "value"], ...processedData];
+
+  return <Chart chartType="Bar" width="100%" height="400px" data={sdata} />;
+});
