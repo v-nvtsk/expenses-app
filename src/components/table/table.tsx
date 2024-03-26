5fdc3a987e82730df80bbdb0214ab144c70efa90
v-nvtsk <v@nvtsk.ru>
2024-03-26 23:14:03 +0300

    feat(component): add - table stats

---
+++ src/components/table/table.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Expense } from "../../store/expenseSlice";
+import { formatMoney } from "../../helpers/format-number/format-number";
+import { Category } from "../../api/expenses-api";
+
+type TableData = {
+  categories: Category[];
+  expenses: Expense[];
+};
+
+type TableProps = {
+  data: TableData;
+};
+
+export const Table = ({ data }: TableProps) =>
+  data.expenses.length > 0 && (
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>title</th>
+          <th>Category</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.expenses.map((item) => (
+          <tr key={item.id}>
+            <td>
+              {new Date(item.creationDate).toLocaleDateString("ru", {
+                year: "numeric",
+                month: "numeric",
+                day: "numeric",
+              })}
+            </td>
+            <td>{item.title}</td>
+            <td>{data.categories.find((c) => c.id === item.categoryId)?.name}</td>
+            <td>{formatMoney(item.amount)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
