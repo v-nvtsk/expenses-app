f69242c3e73a7b2efd8d3c317c9f777af455dbfb
v-nvtsk <v@nvtsk.ru>
2024-03-27 03:49:04 +0300

    fix(app): change BrowserRouter to MemoryRouter in test

--- src/app/index.test.tsx
+++ src/app/index.test.tsx
@@ -1,9 +1,9 @@
 import { act, render, screen, within } from "@testing-library/react";
 import React from "react";
 import { Provider } from "react-redux";
-import { Root } from ".";
+import { MemoryRouter } from "react-router-dom";
 import { Store } from "../store";
-import firebase from "../api/firebase/firebase";
+import App from "./app";
 
 jest.mock("../api/firebase/firebase.ts");
 
@@ -12,7 +12,9 @@ describe("App", () => {
     const app = await act(async () =>
       render(
         <Provider store={Store}>
-          <Root />
+          <MemoryRouter>
+            <App />
+          </MemoryRouter>
         </Provider>,
       ),
     );
@@ -30,10 +32,6 @@ describe("App", () => {
       name: /sign in/i,
     });
     expect(list).toContainElement(linkSignIn);
-
-    jest.spyOn(firebase, "signIn").mockResolvedValue(null);
-    await act(async () => linkSignIn.click());
-    expect(window.location.pathname).toMatch(/\/auth\/signin$/i);
 
     app.unmount();
   });
